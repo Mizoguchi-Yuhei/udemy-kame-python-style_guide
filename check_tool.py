996efaf691f11c1315b299e865d0182f7b467347
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-09-11 23:14:03 +0900

    7-94

---
+++ check_tool.py
@@ -0,0 +1,16 @@
+import os, sys
+x =1
+y =2
+print       (x, y, z)
+
+
+
+# pip install flake8
+# pycodestyleとpyflakesのラッパーライブラリ
+
+# --show-source 問題がある箇所のコードを表示する
+# --show-pep8 該当するpep8の情報を表示する
+# --statistics 統計のみ表示
+# --statistics -qq というように併用することが多い
+
+# pip install pylint
